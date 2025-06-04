28c2b2a74ed0083578cde3b30065079a6467cf95
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2025-06-04 17:05:07 -0300

    Corrigindo as Rotas e atualizando dependência

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 import Login from './pages/Login/Login';
 import CadastroUsuario from './pages/CadastroUsuario/CadastroUsuario';
 import Dashboard from './pages/Dashboard/Dashboard';
-import TesteForm from './pages/TesteForm';
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/teste-form" element={<TesteForm />} />
           </Routes>
         </div>
       </Router>
